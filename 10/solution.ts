f6f6b7d31924a5d22dc91ebb833d100e126a31dc
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2023-12-03 03:17:39 +0500

    Solve puzzle 10.2

--- 10/solution.ts
+++ 10/solution.ts
@@ -2,7 +2,7 @@ export function partOne(text: string): number {
   const stack = text.split("\n");
 
   let x = 1, reg = 0;
-  const strenghts : number[] = [];
+  const strenghts: number[] = [];
 
   let cmd = stack.shift();
   for (let cycle = 1, c = 1; cycle <= 220; ++cycle) {
@@ -10,17 +10,14 @@ export function partOne(text: string): number {
     reg = 0;
     if (cmd?.startsWith("noop")) {
       cmd = stack.shift();
-    }
-    else if (cmd?.startsWith("addx")) {
+    } else if (cmd?.startsWith("addx")) {
       if (c === 2) {
         const v = Number(cmd.split(" ")[1]);
         reg = v;
         cmd = stack.shift();
         c = 1;
-      }
-      else c++;
-    }
-    else throw new Error("Unhandled command");
+      } else c++;
+    } else throw new Error("Unhandled command");
 
     if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
       console.log("Cycle %d: x = %d", cycle, x);
@@ -28,8 +25,42 @@ export function partOne(text: string): number {
     }
   }
 
-  return strenghts.reduce((acc,cur) => acc + cur, 0);
+  return strenghts.reduce((acc, cur) => acc + cur, 0);
 }
-export function partTwo(text: string): number {
-  return 0;
+export function partTwo(text: string): string {
+  const stack = text.split("\n");
+
+  let x = 1, reg = 0;
+  let curFrame = "";
+  const frames = [];
+
+  let cmd = stack.shift();
+  for (let cycle = 1, c = 1, crtp = 0; stack.length > 0; ++cycle, ++crtp) {
+    x += reg;
+    reg = 0;
+    if (cmd?.startsWith("noop")) {
+      cmd = stack.shift();
+    } else if (cmd?.startsWith("addx")) {
+      if (c === 2) {
+        const v = Number(cmd.split(" ")[1]);
+        reg = v;
+        cmd = stack.shift();
+        c = 1;
+      } else c++;
+    } else throw new Error("Unhandled command");
+
+    if (crtp === 40) {
+      crtp = 0;
+      frames.push(curFrame);
+      curFrame = "";
+    }
+
+    if ([x - 1, x, x + 1].includes(crtp)) {
+      curFrame = curFrame.concat("#");
+    }
+    else 
+      curFrame = curFrame.concat(".");
+  }
+  frames.push(curFrame + ".");
+  return frames.join("\n");
 }
