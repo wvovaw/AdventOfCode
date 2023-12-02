9f5614d651eea118890cf1d13ee5af071e2b8461
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2023-12-02 21:52:43 +0500

    Solve puzzle 2.2

--- 02/solution.test.ts
+++ 02/solution.test.ts
@@ -2,13 +2,16 @@ import { assertEquals } from "assert";
 import { partOne, partTwo } from "./solution.ts";
 
 Deno.test("Puzzle 1", async (t) => {
-  const txt1 = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  const txt = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
   await t.step("Part one", async (t) => {
-    assertEquals(partOne(txt1), 8);
+    assertEquals(partOne(txt), 8);
+  });
+  await t.step("Part two", async (t) => {
+    assertEquals(partTwo(txt), 2286);
   });
 });
--- 02/solution.ts
+++ 02/solution.ts
@@ -1,17 +1,19 @@
 type Color = "red" | "green" | "blue";
 
+function getGames(input: string) {
+return input.split("\n").map((line) => {
+    const splitIx = line.indexOf(":") + 2;
+    return line.substring(splitIx);
+  });
+}
+
 export function partOne(text: string): number {
   const CubesLimits: Record<Color, number> = {
     "red": 12,
     "green": 13,
     "blue": 14,
   };
-
-  // Split games removing prefix
-  const games = text.split("\n").map((line) => {
-    const splitIx = line.indexOf(":") + 2;
-    return line.substring(splitIx);
-  });
+  const games = getGames(text);
 
   const impossibleGames = new Set();
   games.forEach((game, ix) => {
@@ -27,9 +29,6 @@ export function partOne(text: string): number {
         if (CubesLimits[color] < count) impossibleGames.add(ix);
       });
     });
-
-    const isImpossible = impossibleGames.has(ix) ? "impossible" : "possible";
-    console.log("Game %d is %s", ix + 1, isImpossible);
   });
 
   const possibleSum = [...Array(games.length).keys()].filter((_, i) => {
@@ -39,5 +38,35 @@ export function partOne(text: string): number {
 }
 
 export function partTwo(text: string) {
-  return 0;
+  const games = getGames(text);
+
+
+  let sum = 0;
+  games.forEach((game, _) => {
+    const turnes = game.split(";");
+
+    const gm : Record<Color, number> = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+
+    turnes.forEach((turn) => {
+      const cubes = turn.split(",").map((cube) => cube.trim());
+
+      cubes.forEach((cube) => {
+        const inp = cube.split(" ");
+        const color: Color = inp[1] as Color;
+        const count = Number(inp[0]);
+
+        if (gm[color] < count) {
+          gm[color] = count;
+        }
+      });
+    });
+
+    sum += gm.red * gm.green * gm.blue;
+  });
+
+  return sum;
 }
