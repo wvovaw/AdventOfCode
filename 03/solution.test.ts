850a9844d84817f0db388499a214ab3c16a76486
Vladimir Ivakhno <vladimir5313375015@gmail.com>
2023-12-04 04:43:05 +0500

    Solve puzzle 03.1

---
+++ 03/solution.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "assert";
+import { partOne, partTwo } from "./solution.ts";
+import readfile from "../lib/readfile.ts";
+
+Deno.test("Puzzle 0", async (t) => {
+  const txt = readfile(import.meta.url, "test.txt");
+
+  await t.step("Part one", async (t) => {
+    assertEquals(partOne(txt), 4361);
+  });
+  await t.step("Part two", async (t) => {
+    assertEquals(partTwo(txt), 0);
+  });
+});
