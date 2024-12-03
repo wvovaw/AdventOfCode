c60e46faa45840df1f02e8ce16bd7356930c40d5
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-12-03 22:42:28 +0500

    Solve puzzle 3.2

--- 02/solution.test.ts
+++ 02/solution.test.ts
@@ -1,11 +1,14 @@
 import { assertEquals } from '@std/assert'
-import { partOne } from './solution.ts'
+import { partOne, partTwo } from './solution.ts'
 import { readfile } from '../lib/textfiels.ts'
 
 Deno.test('Puzzle 0', async (t) => {
-  const txt = readfile(import.meta.url, 'test.txt')
-
   await t.step('Part one', () => {
+    const txt = readfile(import.meta.url, 'test.txt')
     assertEquals(partOne(txt), 161)
+  })
+  await t.step('Part two', () => {
+    const txt = readfile(import.meta.url, 'test2.txt')
+    assertEquals(partTwo(txt), 48)
   })
 })
--- 02/solution.ts
+++ 02/solution.ts
@@ -4,9 +4,11 @@ type Command = (...args: any[]) => any
 const commands = new Map<string, Command>()
 commands.set('add', (x: number, y: number): number => x + y)
 commands.set('mul', (x: number, y: number): number => x * y)
+commands.set("don't", () => 'DONT')
+commands.set('do', () => 'DO')
 
 function interpreter(program: string) {
-  const instructionRx = /(\w+)\(([^)]*)\)/
+  const instructionRx = /([A-Za-z\/\s\.'-]+)\(([^)]*)\)/
   try {
     const match = program.match(instructionRx) || []
     const command = match[1]
@@ -24,6 +26,31 @@ export function partOne(jumbledProgram: string): number {
   const res = instructions
     .map(interpreter)
     .reduce((acc, cur) => acc + cur, 0)
+
+  return res
+}
+
+export function partTwo(jumbledProgram: string): number {
+  const mulRx = /(don?\'?t?)\(\)|(mul)\(\d{1,3}\,\d{1,3}\)/g
+  const instructions = jumbledProgram.match(mulRx) || []
+
+  let lock: boolean = false
+
+  const res = instructions
+    .map(interpreter)
+    .reduce((acc, cur) => {
+      if (typeof cur !== 'number') {
+        if (cur === 'DONT') {
+          lock = true
+        }
+        if (cur === 'DO') {
+          lock = false
+        }
+        return acc
+      } else if (lock) {
+        return acc
+      } else return acc + cur
+    }, 0)
 
   return res
 }
