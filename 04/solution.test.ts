8c5fb1520acddba5635231a694a07544a66573e3
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-12-08 16:13:57 +0500

    Solve puzzle 5.2

--- 04/solution.test.ts
+++ 04/solution.test.ts
@@ -1,10 +1,15 @@
 import { assertEquals } from '@std/assert'
-import { partOne } from './solution.ts'
+import { partOne, partTwo } from './solution.ts'
 import { readfile } from '../lib/textfiels.ts'
 
 Deno.test('Puzzle 0', async (t) => {
   await t.step('Part one', () => {
     const txt = readfile(import.meta.url, 'test.txt')
     assertEquals(partOne(txt), 143)
+  })
+
+  await t.step('Part two', () => {
+    const txt = readfile(import.meta.url, 'test.txt')
+    assertEquals(partTwo(txt), 123)
   })
 })
--- 04/solution.ts
+++ 04/solution.ts
@@ -1,25 +1,87 @@
-export function partOne(text: string): number {
+type Rule = [number, number]
+type Rules = Rule[]
+type Update = number[]
+type Updates = Update[]
+
+function getInputs(text: string): [Rules, Updates] {
   const [orderingText, updateText] = text.split('\n\n')
-  const orderingRules: [number, number][] = orderingText.split('\n')
+  const orderingRules: Rules = orderingText.split('\n')
     .map((r) => {
       const xy = r.split('|')
       return [parseInt(xy[0]), parseInt(xy[1])]
     })
-  const updates: number[][] = updateText.split('\n')
+  const updates: Updates = updateText.split('\n')
     .map((r) => (r.split(',').map(Number)))
+  return [orderingRules, updates]
+}
+
+function testRule(update: Update, rule: Rule): Rule {
+  const x = update.indexOf(rule[0])
+  const y = update.indexOf(rule[1])
+  return [x, y]
+}
+
+function isRuleRelevant(update: Update, rule: Rule): boolean {
+  const [x, y] = testRule(update, rule)
+  return x >= 0 && y >= 0
+}
+
+function isUpdateCorrect(update: Update, orderingRules: Rules): boolean {
+  let isCorrect = true
+  orderingRules.forEach((r) => {
+    if (isRuleRelevant(update, r)) {
+      const [x, y] = testRule(update, r)
+      if (x > y) isCorrect = false
+    }
+  })
+  return isCorrect
+}
+
+function getRelevantRules(update: Update, orderingRules: Rules): Rules {
+  const rules: Rules = []
+  for (const r of orderingRules) {
+    if (isRuleRelevant(update, r)) rules.push(r)
+  }
+  return rules
+}
+
+function fixUpdate(update: Update, orderingRules: Rules): Update {
+  const newUpd = Array.from(update)
+  const relevantRules = getRelevantRules(update, orderingRules)
+
+  while (!isUpdateCorrect(newUpd, relevantRules)) {
+    for (let i = 0; i < relevantRules.length; ++i) {
+      const [x, y] = testRule(newUpd, relevantRules[i])
+      if (x > y) {
+        ;[newUpd[x], newUpd[y]] = [newUpd[y], newUpd[x]]
+        i = 0
+      }
+    }
+  }
+  return newUpd
+}
+
+export function partOne(text: string): number {
+  const [orderingRules, updates] = getInputs(text)
 
   const centers: number[] = []
   for (const upd of updates) {
-    let isCorrect = true
-
-    orderingRules.forEach((r) => {
-      const x = upd.indexOf(r[0])
-      const y = upd.indexOf(r[1])
-
-      if (x >= 0 && y >= 0 && x > y) isCorrect = false
-    })
-
+    const isCorrect = isUpdateCorrect(upd, orderingRules)
     if (isCorrect) centers.push(upd[Math.floor(upd.length / 2)])
+  }
+  return centers.reduce((a, c) => (a + c), 0)
+}
+
+export function partTwo(text: string): number {
+  const [orderingRules, updates] = getInputs(text)
+
+  const centers: number[] = []
+  for (const upd of updates) {
+    const isCorrect = isUpdateCorrect(upd, orderingRules)
+    if (!isCorrect) {
+      const fixedUpd = fixUpdate(upd, orderingRules)
+      centers.push(fixedUpd[Math.floor(upd.length / 2)])
+    }
   }
   return centers.reduce((a, c) => (a + c), 0)
 }
