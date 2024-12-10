e7aed0cd6ade2b7905fe1a5d8ee6038014ca503b
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-12-11 02:16:39 +0500

    Solve puzzle 7.2

--- 06/solution.test.ts
+++ 06/solution.test.ts
@@ -1,10 +1,14 @@
 import { assertEquals } from '@std/assert'
-import { partOne } from './solution.ts'
+import { partOne, partTwo } from './solution.ts'
 import { readfile } from '../lib/textfiels.ts'
 
 Deno.test('Puzzle 6', async (t) => {
   await t.step('Part one', () => {
     const txt = readfile(import.meta.url, 'test.txt')
     assertEquals(partOne(txt), 3749)
+  })
+  await t.step('Part Two', () => {
+    const txt = readfile(import.meta.url, 'test.txt')
+    assertEquals(partTwo(txt), 11387)
   })
 })
--- 06/solution.ts
+++ 06/solution.ts
@@ -1,33 +1,31 @@
-function combinationsReq(length: number, prefix = ''): string[] {
-  if (length === 0) {
-    return [prefix]
-  }
-  return [
-    ...combinationsReq(length - 1, prefix + '*'),
-    ...combinationsReq(length - 1, prefix + '+'),
-  ]
-}
+type Input = [number, number[]][]
 
-export function partOne(text: string): number {
-  const input: [number, number[]][] = text
+function parseInput(text: string): Input {
+  return text
     .split('\n')
     .map((r) => r.split(':'))
     .map((r) => [parseInt(r[0]), r[1].trim().split(' ').map(Number)])
+}
 
+function combinationsReq(length: number, alph: string[], prefix = ''): string[] {
+  if (length === 0) {
+    return [prefix]
+  }
+  return alph.map((ch) => (combinationsReq(length - 1, alph, prefix + ch))).flat()
+}
+
+function evaluator(
+  input: Input,
+  operations: Record<string, (a: number, b: number) => number>,
+): number {
   const sumOfValid = []
   for (const row of input) {
-    const operands = row[1]
-    for (const comb of combinationsReq(operands.length - 1)) {
-      const operators = comb.split('')
-
-      let res = operands[0]
+    const nums = row[1]
+    for (const opsComb of combinationsReq(nums.length - 1, Object.keys(operations))) {
+      const operators = opsComb.split('')
+      let res: number = nums[0]
       for (let i = 0; i < operators.length; ++i) {
-        if (operators[i] === '+') {
-          res += operands[i + 1]
-        }
-        if (operators[i] === '*') {
-          res *= operands[i + 1]
-        }
+        res = operations[operators[i]](res, nums[i + 1])
       }
 
       if (res === row[0]) {
@@ -37,4 +35,21 @@ export function partOne(text: string): number {
     }
   }
   return sumOfValid.reduce((a, c) => a + c, 0)
+}
+
+export function partOne(text: string): number {
+  const input = parseInput(text)
+  return evaluator(input, {
+    '*': (a, b) => a * b,
+    '+': (a, b) => a + b,
+  })
+}
+
+export function partTwo(text: string): number {
+  const input = parseInput(text)
+  return evaluator(input, {
+    '*': (a, b) => a * b,
+    '+': (a, b) => a + b,
+    '~': (a, b) => parseInt(a.toString().concat(b.toString())),
+  })
 }
